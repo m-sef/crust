1633fb198830c66cdeaacfb4973a577ce40c7522
Seth Moore <seth.l.moore0506@gmail.com>
2026-07-06 21:46:54 -0400

    Running new experiment with ebpf_probe

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -16,56 +16,66 @@ run_benchmark() {
 
         TARGET_URL="http://$(sudo kubectl get svc crust-service -n crust -o jsonpath='{.spec.clusterIP}')"
 
+        sudo ebpf_probe --interface=enp6s0f0 &
+        EBPF_PROBE_PID=$!
+
         while true; do
             sudo kubectl get hpa -n crust | tail -n 1 >> "${FOLDER_NAME}/replicas.log" 2>/dev/null
             sleep 1
         done &
         WATCHER_PID=$!
 
+        while true; do
+            sudo cat /sys/fs/bpf/ebpf_probe/cpu*/summary >> "${FOLDER_NAME}/cpu_summary.log" 2>/dev/null
+            sudo cat /sys/fs/bpf/ebpf_probe/rapl/* >> "${FOLDER_NAME}/rapl.log" 2>/dev/null
+            sleep 1
+        done &
+        EBPF_PROBE_WATCHER_PID=$!
+
         # Stable 50 request/sec
-        echo "--- Baseline: 50 req/s (10 minute) ---"
+        echo "--- Baseline: 50 req/s (5 minute) ---"
         echo "GET ${TARGET_URL}:8080/burn?burn=20"| \
-            vegeta attack -rate="50/s" -duration="600s" -keepalive=false | \
+            vegeta attack -rate="50/s" -duration="300s" -keepalive=false | \
             vegeta encode -to=csv -output="${FOLDER_NAME}/vegeta1.log"
         
-        echo "--- Baseline: 75 req/s (10 minute) ---"
+        echo "--- Baseline: 75 req/s (5 minute) ---"
         echo "GET ${TARGET_URL}:8080/burn?burn=20"| \
-            vegeta attack -rate="75/s" -duration="600s" -keepalive=false | \
+            vegeta attack -rate="75/s" -duration="300s" -keepalive=false | \
             vegeta encode -to=csv -output="${FOLDER_NAME}/vegeta2.log"
         
-        echo "--- Baseline: 100 req/s (10 minute) ---"
+        echo "--- Baseline: 100 req/s (5 minute) ---"
         echo "GET ${TARGET_URL}:8080/burn?burn=20"| \
-            vegeta attack -rate="100/s" -duration="600s" -keepalive=false | \
+            vegeta attack -rate="100/s" -duration="300s" -keepalive=false | \
             vegeta encode -to=csv -output="${FOLDER_NAME}/vegeta3.log"
         
-        echo "--- Baseline: 125 req/s (10 minute) ---"
+        echo "--- Baseline: 125 req/s (5 minute) ---"
         echo "GET ${TARGET_URL}:8080/burn?burn=20"| \
-            vegeta attack -rate="125/s" -duration="600s" -keepalive=false | \
+            vegeta attack -rate="125/s" -duration="300s" -keepalive=false | \
             vegeta encode -to=csv -output="${FOLDER_NAME}/vegeta4.log"
         
-        echo "--- Baseline: 150 req/s (10 minute) ---"
+        echo "--- Baseline: 150 req/s (5 minute) ---"
         echo "GET ${TARGET_URL}:8080/burn?burn=20"| \
-            vegeta attack -rate="150/s" -duration="600s" -keepalive=false | \
+            vegeta attack -rate="150/s" -duration="300s" -keepalive=false | \
             vegeta encode -to=csv -output="${FOLDER_NAME}/vegeta5.log"
         
-        echo "--- Baseline: 175 req/s (10 minute) ---"
+        echo "--- Baseline: 175 req/s (5 minute) ---"
         echo "GET ${TARGET_URL}:8080/burn?burn=20"| \
-            vegeta attack -rate="175/s" -duration="600s" -keepalive=false | \
+            vegeta attack -rate="175/s" -duration="300s" -keepalive=false | \
             vegeta encode -to=csv -output="${FOLDER_NAME}/vegeta6.log"
         
-        echo "--- Baseline: 200 req/s (10 minute) ---"
+        echo "--- Baseline: 200 req/s (5 minute) ---"
         echo "GET ${TARGET_URL}:8080/burn?burn=20"| \
-            vegeta attack -rate="200/s" -duration="600s" -keepalive=false | \
+            vegeta attack -rate="200/s" -duration="300s" -keepalive=false | \
             vegeta encode -to=csv -output="${FOLDER_NAME}/vegeta7.log"
 	
-	echo "--- Baseline: 225 req/s (10 minute) ---"
+	echo "--- Baseline: 225 req/s (5 minute) ---"
         echo "GET ${TARGET_URL}:8080/burn?burn=20"| \
-            vegeta attack -rate="225/s" -duration="600s" -keepalive=false | \
+            vegeta attack -rate="225/s" -duration="300s" -keepalive=false | \
             vegeta encode -to=csv -output="${FOLDER_NAME}/vegeta8.log"
 
-	echo "--- Baseline: 250 req/s (10 minute) ---"
+	echo "--- Baseline: 250 req/s (5 minute) ---"
         echo "GET ${TARGET_URL}:8080/burn?burn=20"| \
-            vegeta attack -rate="250/s" -duration="600s" -keepalive=false | \
+            vegeta attack -rate="250/s" -duration="300s" -keepalive=false | \
             vegeta encode -to=csv -output="${FOLDER_NAME}/vegeta9.log"
         ## Ramp up 120 request/sec
         #echo "--- Spike: 120 req/s (3 minutes) ---"
@@ -83,12 +93,14 @@ run_benchmark() {
         #sleep 5m
 
         kill ${WATCHER_PID}
+        kill ${EBPF_PROBE_PID}
+        kill ${EBPF_PROBE_WATCHER_PID}
 
         sudo kubectl delete -f $YAML
         sudo kubectl wait --for=delete pod --all -n crust --timeout=60s
     done
 }
 
-run_benchmark default_hpa_experimental $SCRIPT_DIR/../yaml/crust.yaml 1
+run_benchmark default_hpa_experimental2 $SCRIPT_DIR/../yaml/crust.yaml 1
 
 exit 0
